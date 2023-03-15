afe9b07db113662a085eedbd86c3a104231a4ac7
TawanaState <88168991+TawanaState@users.noreply.github.com>
2023-03-15 04:55:34 +0200

    Upload

---
+++ assets/js/db.js
@@ -0,0 +1,282 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDrZEkLglljvwXED1RNQlWF256SiQErVlE",
+  authDomain: "campuscompass-52b4e.firebaseapp.com",
+  projectId: "campuscompass-52b4e",
+  storageBucket: "campuscompass-52b4e.appspot.com",
+  messagingSenderId: "1065427206502",
+  appId: "1:1065427206502:web:d3034333b69b5d114808a7",
+  measurementId: "G-E7BG9XL8DV"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+var user = {};
+var friends = [];
+var events = [];
+
+
+
+function refreshUI() {
+
+  try {
+    document.querySelectorAll("datalist#Places > option[type='Friend']").forEach((v, k, p) => {
+      v.remove();
+    });
+    document.querySelectorAll("datalist#Places > option[type='Event']").forEach((v, k, p) => {
+      v.remove();
+    });
+  friends.forEach((v, k, p) => {
+    let li = document.createElement('li');
+    li.innerHTML = `<p>${v.data.username}</p> <span>${new Date(v.data.lastseen).toString()}</span>`;
+    li.onclick = function (event) {
+      window.location.href = `index.html#friend=${v.key}`;
+    }
+    document.querySelector('ul.list.friends').appendChild(li);
+
+
+
+    
+    let op = document.createElement('option');
+    op.value = '@' + v.data.username;
+    op.setAttribute('type', 'Friend');
+    op.innerText = v.data.lastseen;
+    select('datalist#Places').appendChild(op);
+
+
+
+  });
+
+  events.forEach((v, k, p) => {
+    let startTime = new Date(v.data.startTime);
+    let endTime = new Date(v.data.endTime);
+    let now = new Date();
+  
+    if (now > startTime && endTime > now) {
+      let li = document.createElement("li");
+      li.innerHTML = `<p>${v.data.event_name}</p> <span>${new Date(
+        v.data.startTime
+      ).toLocaleTimeString()} - ${new Date(
+        v.data.endTime
+      ).toLocaleTimeString()}</span>`;
+      select("ul.list.events").appendChild(li);
+
+
+      let op = document.createElement('option');
+      op.value = '~' + v.data.event_name;
+      op.setAttribute('type', 'Event');
+      op.innerText = new Date(v.data.startTime).toLocaleTimeString() + " - " + new Date(v.data.endTime).toLocaleTimeString();
+      select('datalist#Places').appendChild(op);
+
+    }
+  });
+  
+  events.forEach((v, k, p) => {
+    let startTime = new Date(v.data.startTime);
+    let endTime = new Date(v.data.endTime);
+  
+    if (v.data.user_uid == user.uid) {
+      let li = document.createElement("li");
+      li.classList.add('edit');
+      li.innerHTML = `<p><span>${v.data.event_name}</span><span>${startTime.toLocaleTimeString()} - ${endTime.toLocaleTimeString()}</span></p>
+      <p style="flex-direction: row;"><i class="bi bi-pencil-fill" onclick="openPrompt('${v.key}')"></i><i onclick="shareEvent('${v.key}')" class="bi bi-share"></i><i class="bi bi-trash" onclick="removeEvent('${v.key}')"></i></p>`;
+      select("ul.list.my-events").appendChild(li);
+    }
+  });
+
+  } catch (error) {
+  }
+  
+}
+
+refreshUI();
+var database = firebase.database();
+
+var eventsListRef = database.ref("events");
+var friendsListRef = database.ref("friends");
+
+// friend => |user_uid|, lastseen, username, location{}
+// event => |uid|, startTime, endTime, user_uid, location{}, event_name, description, images[];
+
+firebase.auth().onAuthStateChanged((userr) => {
+  if (userr) {
+    user = userr;
+    var uid = userr.uid;
+    // ...
+  } else {
+    if(!window.location.href.includes('signup') && !window.location.href.includes('login')){
+      window.location.href = "login.html";
+    }
+  }
+
+  try {
+    document.querySelector('#heading_username').innerText = user.displayName;
+  } catch (error) {
+  }
+
+});
+
+eventsListRef.on('value', (snapshot) => {
+  events = [];
+  snapshot.forEach((childSnapshot) => {
+    events.push({
+      key : childSnapshot.key,
+      data : childSnapshot.val()
+    });
+  });
+  refreshUI();
+});
+
+friendsListRef.on('value', (snapshot) => {
+  friends = [];
+  snapshot.forEach((childSnapshot) => {
+    friends.push({
+      key : childSnapshot.key,
+      data : childSnapshot.val()
+    });
+  });
+  refreshUI();
+});
+
+
+function SignUp(email, password, username) {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      user = firebase.auth().currentUser;
+      addFriend({
+        user_id: user.uid,
+        lastseen: new Date().toString(),
+        location: { lat: -17.78521, lng: 31.05311 },
+        username: username,
+      });
+      user
+        .updateProfile({
+          displayName: username,
+        })
+        .then(() => {
+          // Update successful
+          window.location.href = "index.html";
+
+          // ...
+        })
+        .catch((error) => {
+          // An error occurred
+          window.setTimeout(() => {
+            window.location.href = "index.html";
+          }, 1000);
+          return "Error in editing username";
+          // ...
+        });
+    })
+    .catch((error) => {
+      notify(error.message, ["text-danger"]);
+      // ..
+    });
+}
+function Login(email, password) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      user = userCredential.user;
+      window.location.href = "index.html";
+      // ...
+    })
+    .catch((error) => {
+      return error.message;
+    });
+}
+function addEvent(param) {
+  try {
+  let newEvent = eventsListRef.push();
+  newEvent.set(param);
+  return newEvent.key;
+  } catch (error) {
+    notify(error)
+  }
+}
+function addFriend(param) {
+  try {
+  let newFriend = friendsListRef.push();
+  newFriend.set(param);
+  return newFriend.key;
+  } catch (error) {
+    notify(error)
+  }
+}
+function removeEvent(uid) {
+  try {
+    database.ref("events/" + uid).remove();
+  } catch (error) {
+    notify(error, 'text-danger');
+  }
+  
+}
+function getFriend(key, myfunc = (()=> {})) {
+  friendsListRef.child(key).get().then((snapshot) => {
+    if (snapshot.exists()) {
+      myfunc(snapshot.val())
+      return snapshot.val();
+    } else {
+      notify("No data available.", 'text-warning');
+      return null;
+    }
+  }).catch((error) => {
+    notify(error, 'text-warning');
+  });
+}
+function getEvent(key, myfunc = (() => {})) {
+  eventsListRef.child(key).get().then((snapshot) => {
+    if (snapshot.exists()) {
+      myfunc(snapshot.val())
+      return snapshot.val();
+    } else {
+      notify("No data available.", 'text-warning');
+      return null;
+    }
+  }).catch((error) => {
+    notify(error, 'text-warning');
+  });
+}
+function updateEvent(uid, param) {
+  try {
+  let evRef = database.ref("friends/" + uid);
+  evRef.update(param);
+  } catch (error) {
+    notify(error)
+  }
+}
+function updateFriendLocation(position) {
+  try {
+  let userRef = database.ref("friends/" + friends.filter(val => val.data.user_id == user.uid)[0].key);
+  userRef.update({
+    lastseen: new Date().toString(),
+    location: {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    },
+  });
+  } catch (error) {
+    notify(error)
+  }
+}
+
+
+if (navigator.geolocation) {
+  //after the user indicates that they want to turn on continuous location-tracking
+  var watchId = navigator.geolocation.watchPosition(
+    updateFriendLocation,
+    geolocationFailure
+  );
+} else {
+  notify("Geolocation is not supported by this browser.", ["text-danger"]);
+}
+
+
+function geolocationFailure() {
+  notify('Please turn on your location for location tracking.', ['text-danger']);
+}
---
+++ assets/js/maps.js
@@ -0,0 +1,398 @@
+var map, watchId, userPin, directionsManager, routePath, destinationLoc, friendPin, EventPin, PlacePin;
+
+function init() {
+    map = new Microsoft.Maps.Map("#map", {
+      center: new Microsoft.Maps.Location(-17.78521, 31.05311),
+      zoom: 19
+    });
+
+    friendPin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-17.78521, 31.05311), {visible : false});
+    EventPin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-17.78521, 31.05311), {visible : false});
+    PlacePin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-17.78521, 31.05311), {visible : false});
+    map.entities.push(friendPin);
+    map.entities.push(EventPin);
+    map.entities.push(PlacePin);
+
+
+
+  //Load the directions and spatial math modules.
+  Microsoft.Maps.loadModule(["Microsoft.Maps.Directions"], function () {
+    //Create an instance of the directions manager.
+    directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
+
+    //Define direciton options that you want to use, that won't be reset the next time a route is calculated.
+
+    //Set the request options that avoid highways and uses kilometers.
+    directionsManager.setRequestOptions({
+      distanceUnit: Microsoft.Maps.Directions.DistanceUnit.km,
+      routeAvoidance: [
+        Microsoft.Maps.Directions.RouteAvoidance.avoidLimitedAccessHighway,
+      ],
+    });
+
+    //Make the route line thick and green.
+    directionsManager.setRenderOptions({
+      drivingPolylineOptions: {
+        strokeColor: "mediumaquamarine",
+        strokeThickness: 6,
+      },
+    });
+
+    Microsoft.Maps.Events.addHandler(
+      directionsManager,
+      "directionsUpdated",
+      directionsUpdated
+    );
+  });
+}
+
+window.places = [
+  {
+    title: "University Of Zimbabwe",
+    position: { lat: -17.78521, lng: 31.05311 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Administration",
+    position: { lat: -17.7813949, lng: 31.0540614 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Library",
+    position: { lat: -17.7804668, lng: 31.053666 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Swimming Pool",
+    position: { lat: -17.7833028, lng: 31.0548178 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "University Of Zimbabwe BEIT HALL",
+    position: { lat: -17.7824513, lng: 31.0538720 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Senior Common Room",
+    position: { lat: -17.7832865, lng: 31.0542170 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "University Of Zimbabwe Christian Union",
+    position: { lat: -17.7832128, lng: 31.0534881 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Sports Pavilion",
+    position: { lat: -17.7849074, lng: 31.0536728 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Faculty Of Science",
+    position: { lat: -17.7849061, lng: 31.0545811 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Law Library",
+    position: { lat: -17.7804304, lng: 31.0532071 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Great Hall",
+    position: { lat: -17.7808729, lng: 31.0541888 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "New Llewelyn Block",
+    position: { lat: -17.7811449, lng: 31.0528340 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ LTI LTII",
+    position: { lat: -17.7803908, lng: 31.0523529 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "University Of Zimbabwe, Diamond Lecture Theatre",
+    position: { lat: -17.7822285, lng: 31.052464 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Diamond Cafe",
+    position: { lat: -17.7825264, lng: 31.0532900 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Department of Architure and Real Estate",
+    position: { lat: -17.7828443, lng: 31.05311679 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Geology Department",
+    position: { lat: -17.7825861, lng: 31.0525165 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Faculty Of Computer Science",
+    position: { lat: -17.7834768, lng: 31.0507295 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Computer Center",
+    position: { lat: -17.7835994, lng: 31.0509306 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "University Of Zimbabwe, Mathematics Department",
+    position: { lat: -17.7836661, lng: 31.0512377 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Johnson Siziba",
+    position: { lat: -17.7831240, lng: 31.0512438 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "University Of Zimbabwe, Physics Department",
+    position: { lat: -17.7822317, lng: 31.0514667 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Security Control Room",
+    position: { lat: -17.7801705, lng: 31.0500368 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Agriculture",
+    position: { lat: -17.7819539, lng: 31.0505226 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ FACULTY OF ENGINEERING",
+    position: { lat: -17.7818588, lng: 31.0491798 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Engineering, Mining and Metallurgy",
+    position: { lat: -17.7825391, lng: 31.0490658 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Engineering Workshops",
+    position: { lat: -17.7818240, lng: 31.0482568 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Medicine",
+    position: { lat: -17.7841153, lng: 31.0492797 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Faculty of Medicine and Health Sciences",
+    position: { lat: -17.7845326, lng: 31.0494225 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ Veterinary Science",
+    position: { lat: -17.7854067, lng: 31.0490155 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "Faculty of Veterinary Science",
+    position: { lat: -17.7860995, lng: 31.0492717 },
+    content: "",
+    images: [],
+  },
+  {
+    title: "UZ HLT 200, HLT 300, HLT 500",
+    position: { lat: -17.7801881, lng: 31.0513702 },
+    content: "",
+    images: [],
+  },
+  
+];
+
+window.init = init;
+
+function startTracking() {
+  //Add a pushpin to show the user's location.
+  userPin = new Microsoft.Maps.Pushpin(map.getCenter(), { visible: false });
+  map.entities.push(userPin);
+
+  //Watch the users location.
+  watchId = navigator.geolocation.watchPosition(usersLocationUpdated);
+}
+
+function locateFriend(param) {
+  let loc = new Microsoft.Maps.Location(
+    param.location.latitude,
+    param.location.longitude
+  );
+  friendPin.setLocation(loc);
+  //Create custom Pushpin
+  friendPin.setOptions({
+      title: param.username,
+      subTitle: new Date(param.lastseen).toString(),
+      text: 'f',
+      visible : true
+  });
+
+  //Center the map on the user's location.
+  map.setView({ center: loc });
+  showinFooter('@'+param.username,'<b>Lastseen : </b> ' + new Date(param.lastseen).toString(), [], param.location)
+
+}
+
+function locateEvent(param) {
+  let loc = new Microsoft.Maps.Location(
+    param.location.latitude,
+    param.location.longitude
+  );
+  EventPin.setLocation(loc);
+  //Create custom Pushpin
+  EventPin.setOptions({
+      title: param.event_name,
+      subTitle: 'event',
+      text: 'e',
+      visible : true
+  });
+
+
+
+  //Center the map on the user's location.
+  map.setView({ center: loc });
+  showinFooter('~'+param.event_name, param.description, param.images, param.location);
+
+}
+
+function locatePlace(param) {
+  let loc = new Microsoft.Maps.Location(
+    param.position.lat,
+    param.position.lng
+  );
+  PlacePin.setLocation(loc);
+  //Create custom Pushpin
+  PlacePin.setOptions({
+      title: param.title,
+      subTitle: 'event',
+      text: 'e',
+      visible : true
+  });
+
+
+
+  //Center the map on the user's location.
+  map.setView({ center: loc });
+  showinFooter(param.title, param.content, param.images, {latitude : param.position.lat, longitude : param.postion.lng});
+
+}
+
+function usersLocationUpdated(position) {
+  var loc = new Microsoft.Maps.Location(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+
+  //Update the user pushpin.
+  userPin.setLocation(loc);
+  userPin.setOptions({ visible: true });
+
+  //Center the map on the user's location.
+  map.setView({ center: loc });
+
+  //Calculate a new route if one hasn't been calculated or if the users current location is further than 50 meters from the current route.
+  if (!routePath || Microsoft.Maps.SpatialMath.distance(loc, routePath) > 50) {
+    calculateRoute(loc, destinationLoc);
+  }
+}
+
+function stopTracking() {
+  // Cancel the geolocation updates.
+  navigator.geolocation.clearWatch(watchId);
+
+  //Remove the user pushpin.
+  map.entities.clear();
+  map.entities.push(friendPin);
+  map.entities.push(EventPin);
+  map.entities.push(PlacePin);
+  clearDirections();
+}
+
+function calculateRoute(userLocation, destination) {
+  clearDirections();
+
+  //Create waypoints to route between.
+  directionsManager.addWaypoint(
+    new Microsoft.Maps.Directions.Waypoint({ location: userLocation })
+  );
+  directionsManager.addWaypoint(
+    new Microsoft.Maps.Directions.Waypoint({ location: destination })
+  );
+
+  //Calculate directions.
+  directionsManager.calculateDirections();
+}
+
+function directionsUpdated(e) {
+  //When the directions are updated, get a polyline for the route path to perform calculations against in the future.
+  var route = directionsManager.getCurrentRoute();
+
+  if (route && route.routePath && route.routePath.length > 0) {
+    routePath = new Microsoft.Maps.Polyline(route.routePath);
+  }
+}
+
+function clearDirections() {
+  //Clear directions waypoints and display without clearing it's options.
+  directionsManager.clearDisplay();
+
+  var wp = directionsManager.getAllWaypoints();
+  if (wp && wp.length > 0) {
+    for (var i = wp.length - 1; i >= 0; i--) {
+      this.directionsManager.removeWaypoint(wp[i]);
+    }
+  }
+
+  routePath = null;
+}
+
+function goNow(event) {
+    destinationLoc = new Microsoft.Maps.Location(
+        Number(document.querySelector('#LocPin').getAttribute('lat')),
+        Number(document.querySelector('#LocPin').getAttribute('lng'))
+      );
+      startTracking();
+}
+function shareNow(event) {
+    
+}
